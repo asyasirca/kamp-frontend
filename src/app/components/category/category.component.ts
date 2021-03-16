3bfd90dad8d07ef86815cac9a5826446f2885583
A GÜL <asya0077@gmail.com>
2021-03-17 00:32:08 +0300

    13 mart tamamlandı

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -33,4 +33,13 @@ export class CategoryComponent implements OnInit {
     }
   }
 
+  getAllCategoryClass(){
+    if(!this.currentCategory){
+     return "list-group-item active"
+    }
+    else{
+     return "list-group-item"
+    }
+  }
+
 }
