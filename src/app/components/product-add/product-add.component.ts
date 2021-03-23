1b31733310b1553aaf328c9e41c9504f26671374
A GÜL <asya0077@gmail.com>
2021-03-24 01:20:41 +0300

    ders 20 tamamlandı

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -32,23 +32,24 @@ export class ProductAddComponent implements OnInit {
     if(this.productAddForm.valid){
       let productModel = Object.assign({},this.productAddForm.value)
       this.productService.add(productModel).subscribe(response=>{
-        console.log(response)
+        
         this.toastrService.success(response.message,"Başarılı")
 
       },responseError=>{
-        console.log(responseError)
-        this.toastrService.error(responseError.error)
-        
+        if(responseError.error.Errors.length>0){
+          for (let i = 0; i < responseError.error.Errors.length; i++) {
+            this.toastrService.error(responseError.error.Errors[i].ErrorMessage,
+              "Doğrulama Hatası")
+            
+          }
+          
+        }
+
       })
     }else{
       this.toastrService.error("Formunuz eksik","Dikkat!")
 
     }
-      
-
-    
-    
-    
   }
 
 }
