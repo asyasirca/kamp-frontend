8e012f0ed6bb6795eb5c67a2284a5b751465f0bc
A GÜL <asya0077@gmail.com>
2021-03-28 00:54:39 +0300

    24 mart,ders 21,bölüm 2

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl,Validators,FormBuilder } from "@angular/forms";
+import {FormGroup,FormControl, Validators, FormBuilder  } from "@angular/forms";
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,32 +9,35 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
 
-  constructor(private formBuilder:FormBuilder, private authService:AuthService) { }
+  loginForm:FormGroup;
+  constructor(private formBuilder:FormBuilder,
+     private authService:AuthService, private toastrService:ToastrService) { }
 
   ngOnInit(): void {
     this.createLoginForm();
-}
-createLoginForm(){
-     
-  this.loginForm = this.formBuilder.group({
-    email: ["",Validators.required],
-    password:["", Validators.required],
-
-
-     }) 
-}
-login(){
-  if (this.loginForm.valid) {
-    console.log(this.loginForm.value);
-    let loginModel = Object.assign({},this.loginForm.value)
-
-    this.authService.login(loginModel).subscribe(data=>{
-      console.log(data)
-    })
-    
   }
-}
+
+  createLoginForm(){
+    this.loginForm = this.formBuilder.group({
+      email: ["",Validators.required],
+      password:["",Validators.required]
+    })
+  }
+
+  login(){
+    if(this.loginForm.valid){
+      
+      let loginModel = Object.assign({},this.loginForm.value)
+
+      this.authService.login(loginModel).subscribe(response=>{
+        this.toastrService.info(response.message)
+        localStorage.setItem("token",response.data.token)
+      },responseError=>{
+        //console.log(responseError)
+        this.toastrService.error(responseError.error)
+      })
+    }
+  }
 
 }
